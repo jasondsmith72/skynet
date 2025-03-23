ba7db7f7375e996bc432360f94655aca1c34e60e
jasondsmith72 <jason.smith@mtusa.com>
2025-03-23 15:50:09 -0700

    Add init file for Clarity compiler package

--- src/clarity/compiler/__init__.py
+++ src/clarity/compiler/__init__.py
@@ -1,1 +1,16 @@
-# Clarity compiler package initialization
+"""
+Clarity Programming Language Compiler
+
+This package contains the core components of the Clarity compiler.
+"""
+
+from .lexer import tokenize, Token, TokenType
+from .parser import parse
+from .semantic_analyzer import SemanticAnalyzer, SemanticError
+import parser_expressions  # This will apply the extensions to the Parser class
+
+__all__ = [
+    'tokenize', 'Token', 'TokenType', 
+    'parse',
+    'SemanticAnalyzer', 'SemanticError'
+]
