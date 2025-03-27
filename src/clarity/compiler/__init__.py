692615d24b02e5b15e7f6687d1b62f1260b80cce
jasondsmith72 <jason.smith@mtusa.com>
2025-03-26 22:59:07 -0700

    feat: Import parser_statements in compiler package

--- src/clarity/compiler/__init__.py
+++ src/clarity/compiler/__init__.py
@@ -8,6 +8,7 @@ from .lexer import tokenize, Token, TokenType
 from .parser import parse
 from .semantic_analyzer import SemanticAnalyzer, SemanticError
 import parser_expressions  # This will apply the extensions to the Parser class
+import parser_statements # This will apply the statement parsing extensions
 
 __all__ = [
     'tokenize', 'Token', 'TokenType', 
